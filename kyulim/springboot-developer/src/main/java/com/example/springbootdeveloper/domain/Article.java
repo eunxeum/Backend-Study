6cb5cbc1d5560eaccf8b24cf7b4a58e56a999d19
ckr8305 <ckr8305@naver.com>
2025-01-14 00:05:35 +0900

    레벨2/7장

--- kyulim/springboot-developer/src/main/java/com/example/springbootdeveloper/domain/Article.java
+++ kyulim/springboot-developer/src/main/java/com/example/springbootdeveloper/domain/Article.java
@@ -2,14 +2,20 @@ package com.example.springbootdeveloper.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@EntityListeners(AuditingEntityListener.class) // 엔티티 생성 및 수정 시간 자동으로 감시 및 기록
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -24,6 +30,14 @@ public class Article {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @CreatedDate // 엔티티가 생성될 때 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 엔티티가 수정될 때 수정 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder // 빌더 패턴으로 객체 생성
     public Article(String title, String content) {
