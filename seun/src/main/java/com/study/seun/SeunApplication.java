9496f81a70f105c3aa980eabd99f49ddb3adeac0
Park, seun <seunpark04@gmail.com>
2025-01-16 16:29:33 +0900

    7장 실습

--- seun/src/main/java/com/study/seun/SeunApplication.java
+++ seun/src/main/java/com/study/seun/SeunApplication.java
@@ -2,7 +2,9 @@ package com.study.seun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //create_at, updated_at 자동 업데이트
 @SpringBootApplication
 public class SeunApplication {
 
