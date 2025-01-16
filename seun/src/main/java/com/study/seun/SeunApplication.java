85f6523237a0a25e6b317a0ec68e0e91422a691c
Park, seun <seunpark04@gmail.com>
2025-01-16 15:56:30 +0900

    6장 실습

---
+++ seun/src/main/java/com/study/seun/SeunApplication.java
@@ -0,0 +1,13 @@
+package com.study.seun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeunApplication.class, args);
+	}
+
+}
